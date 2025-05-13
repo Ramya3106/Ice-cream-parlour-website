6e20356fe1b8be7068e166232552322176fb3570
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-05-13 13:48:29 +0530

    initialized

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[80px]'>
+        <div className=''>
+            <span className='text-2xl text-red-600 font-medium cursor-pointer'>ICEPARLOUR</span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
